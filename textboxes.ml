a9bfad6f8305c548bf5883f55435e23076226347
masternox <masternox@users.noreply.github.com>
2012-10-16 23:07:50 +0000

    -

--- textboxes.ml
+++ textboxes.ml
@@ -1,1 +1,8 @@
-/* The following functions and definitions will be used to detect and display bounding boxes around the text of our image */
+(* The following functions and definitions will be used
+to detect and display bounding boxes around the text of our image *)
+
+let get_pix img x y = Sdlvideo.get_pixel_color img x y
+
+(* Great optimisation ! *)
+
+(* More code coming here very soon *)
