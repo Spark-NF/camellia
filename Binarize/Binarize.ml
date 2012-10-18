0a809c3b82d0d55e302aedaa95fe1ae1cea773eb
Sébastien <trouve_s@epita.fr>
2012-10-18 22:56:45 +0000

    -

--- Binarize/Binarize.ml
+++ Binarize/Binarize.ml
@@ -28,15 +28,49 @@ let color2grey c =
   let x = int_of_float (255. *. level c) in
   (x,x,x)
 
-let image2grey imgRGB imgG w h moy =
+let image2grey imgRGB imgG w h moy min max =
   begin
     for i = 0 to h do
       for j = 0 to w do
 	moy:= (!moy +. level (Sdlvideo.get_pixel_color imgRGB j i));
 	    Sdlvideo.put_pixel_color imgG j i 
 	        (color2grey (Sdlvideo.get_pixel_color imgRGB j i));
+	let (x,_,_) = (Sdlvideo.get_pixel_color imgRGB j i) in
+	if x < !min then min := x
+        else if x > !max then max := x;
       done
     done
+  end
+
+let resize_spec imgG w h moy min max =
+  begin
+    moy := float_of_int(int_of_float(!moy)- !min)/. float_of_int !max *. 255.;
+    for i = 0 to h do
+      for j = 0 to w do
+	let (x,_,_) = (Sdlvideo.get_pixel_color imgG j i) in
+	let y = 
+	  int_of_float((float_of_int( x - !min)/. float_of_int !max)*. 255.)
+	  in
+	Sdlvideo.put_pixel_color imgG j i (y,y,y);
+      done
+    done
+  end
+
+let clean_pix img w h =
+  begin
+    for i = 1 to (h-1) do
+      for j = 1 to (w-1) do
+	let (x,_,_) = Sdlvideo.get_pixel_color img j i in
+	if x = 0 then
+	  let (y,_,_) = Sdlvideo.get_pixel_color img (j-1) (i)
+	  and (z,_,_) = Sdlvideo.get_pixel_color img (j+1) (i) 
+	  and (u,_,_) = Sdlvideo.get_pixel_color img (j) (i-1)
+	  and (v,_,_) = Sdlvideo.get_pixel_color img (j) (i+1) in
+	    if (u != 0 && v != 0) || (y != 0 && z != 0) then
+	      Sdlvideo.put_pixel_color img j i (255,255,255);
+      done
+    done;
+    img; 
   end
 
 let rec fusion l1 l2 = match (l1,l2) with 
@@ -89,12 +123,12 @@ let cleanimg imgG imgC w h =
   end
 
 let grey2black img w h moy =
-  let (x,_,_) = Sdlvideo.get_pixel_color img 1 1 in
-  if x > moy then
+  let (u,_,_) = Sdlvideo.get_pixel_color img 1 1 in
+  if u > moy then
   for i = 0 to h do
     for j = 0 to w do
       let (x,_,_) = Sdlvideo.get_pixel_color img j i in
-      if x > moy - (moy/7) then 
+      if x > moy - (moy/5) then 
 	Sdlvideo.put_pixel_color img j i (255,255,255)
       else 
 	Sdlvideo.put_pixel_color img j i (0,0,0)
@@ -104,7 +138,7 @@ let grey2black img w h moy =
   for i = 0 to h do
     for j = 0 to w do
       let (x,_,_) = Sdlvideo.get_pixel_color img j i in
-      if x < moy + ((255-moy)/7) then 
+      if x < moy + ((255-moy)/5) then 
 	Sdlvideo.put_pixel_color img j i (255,255,255)
       else 
 	Sdlvideo.put_pixel_color img j i (0,0,0)
@@ -116,11 +150,15 @@ let binarize arg =
     let img = Sdlloader.load_image arg in
     let (w,h) = get_dims img in
     let imgG = Sdlvideo.create_RGB_surface_format img [] w h in
-    let moy = ref 0. in
-    let _ = image2grey img imgG w h moy in
+    let moy = ref 0. 
+    and min = ref 0 
+    and max = ref 0 in
+    let _ = image2grey img imgG w h moy min max in
     let moyenne = (int_of_float (!moy *. 255.)) / (w*h) in
-    let imgC = cleanimg imgG img w h in
-    let _ = grey2black imgC w h moyenne in
+    let _ = resize_spec imgG w h moy min max in
+    let _ = grey2black imgG w h moyenne in
+    let imgC = clean_pix imgG w h in
+    (*let imgC = cleanimg imgC img w h in*)
     imgC
   end
 
