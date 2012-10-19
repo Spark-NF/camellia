1519d1834c49d0aa5ba50be014b35de767b4d6df
Nicolas Faure <nicolas.faure.7@gmail.com>
2012-10-19 21:45:57 +0000

    -

--- Makefile
+++ Makefile
@@ -3,7 +3,7 @@ OCAMLFLAGS	:= -I +sdl
 OCAMLLD			:= bigarray.cmxa sdl.cmxa sdlloader.cmxa
 
 tpsdl:	skew.ml
-	${OCAML} ${OCAMLFLAGS} ${OCAMLLD} -o camellia skew.ml
+	${OCAML} ${OCAMLFLAGS} ${OCAMLLD} -o camellia Sdl.ml Skew.ml main.ml
 
 clean::
 	rm -f *~ *.o *.cm? test
--- Skew.ml
+++ Skew.ml
@@ -1,19 +1,17 @@
-let get_dimensions img = (10, 10) ;;
-let get_pixel img x y = 1 ;;
-
-
-
 (* Trucs et choses *)
 
 let _PIXEL_COUNT = 2 ;;
+let _PI = 3.14159265359 ;;
 
 
 
 (* Détecte l'angle de l'image *)
 
+let get_pixel img x y =
+	Sdlvideo.get_pixel_color img x y = (0, 0, 0) ;;
+
 let skew img =
 	let (w, h) = get_dimensions img in
-	let angle = 0.0 in
 	let first_left = ref (-1, -1) in
 	let first_top = ref (-1, -1) in
 	begin
@@ -57,6 +55,26 @@ let skew img =
 
 (* Rotate l'image *)
 
-let rotate img =
-	let (w, h) = get_dimensions img in
-	img ;;
+let rotate img angle =
+	if angle = 0.0 then
+		img
+	else
+		let (width, height) = get_dimensions img in
+		let x_m = width / 2 and y_m = height / 2 in
+		let x_d = ref 0 and y_d = ref 0 in
+		let dest = Sdlvideo.create_RGB_surface_format img [] width height in
+		begin
+			for y_s = 0 to height do
+				for x_s = 0 to width do
+					begin
+						y_d :=	int_of_float (((float_of_int (y_s - y_m)) *. (cos angle)) -.
+									((float_of_int (x_s - x_m)) *. (sin angle))) + y_m;
+						x_d :=	int_of_float (((float_of_int (y_s - y_m)) *. (sin angle)) +.
+									((float_of_int (x_s - x_m)) *. (cos angle))) + x_m;
+						if (!y_d < height) && (!y_d >= 0) && (!x_d < width) && (!x_d >= 0) then
+							Sdlvideo.put_pixel_color dest !x_d !y_d (Sdlvideo.get_pixel_color img x_s y_s);
+					end
+				done
+			done;
+			dest
+		end ;;
