c1d7a791c8e544a2bee0e2baf3bfc554ceebc968
masternox <masternox@users.noreply.github.com>
2012-10-20 01:44:34 +0000

    -

--- XY-Cut Improved/textboxes.ml
+++ XY-Cut Improved/textboxes.ml
@@ -1,5 +1,3 @@
-
-
 (*
 let img_size = (100, 100)
 let img_size_x = let (x, y) = img_size in x
@@ -7,46 +5,74 @@ let img_size_y = let (x, y) = img_size in y
 *)
 
 (* Returns a boolean equals to true if the pixel is black *)
+
 let is_pixel_black x y =
-	let color = Sdlvideo.get_pixel_color img x y in
-		color = (0, 0, 0)
+    let color = Sdlvideo.get_pixel_color img x y in
+        color = (0, 0, 0)
 ;;
 
 (* Returns the number of black pixels in the line between the two x bounds *)
-let rec y_pix_num y x1 x2 = match x1 with
-	| x2 -> 0
-	| x -> (if (is_pixel_black x y) then 1 else 0)
-		+ y_pix_num y (x+1) x2
+
+let rec x_hist y x1 x2 = match x1 with
+    | x2 -> 0
+    | x -> (if (is_pixel_black x y) then 1 else 0)
+        + x_hist y (x+1) x2
 ;;
 
 (* Returns the number of black pixels in the column between the two y bounds *)
-let rec x_pix_num x y1 y2 = match y1 with
-	| y2 -> 0
-	| y -> (if (is_pixel_black x y) then 1 else 0)
-		+ x_pix_num x (y+1) y2
+
+let rec y_hist x y1 y2 = match y1 with
+    | y2 -> 0
+    | y -> (if (is_pixel_black x y) then 1 else 0)
+        + y_hist x (y+1) y2
 ;;
+
 
 (* Returns the a list of quadra-int (x1, x2, y1, y2) formed by horizontal cuts
 in the given box *)
-let rec x_cut bounds =
-	let (x1, x2, y1, y2) = bounds in
-	
 
-(*
-Returns the tree with the following caracteristics :
- - It's key is a quadra-int (x1, x2, y1, y2) representing the box bounds
- - It's sons are boxes formed by horizontal or vertical cuts in the parent box
-*)
-let rec xy_cut order T =
-	let cuts_list = match order with
-		| 0 -> x_cut T.key
-		| 1 -> y_cut T.key in
-	if not (is_empty cuts_list) then
-		match cuts_list with
-		| [] -> T
-		| n :: l ->
-			begin
-			T.son[last_son +1] := n
-			xy_cut (abs (order -1)) T.son[last_son]
-			end
+Type  quadra_tree = Empty | Node of (int, int, int, int) * quadra_tree list
 ;;
+
+let boxes_tree = ref (x1, x2, y1, y2)
+val boxes_tree : quadra_tree ref = {contents = (x1, x2, y1, y2)}
+
+let tree_list = ref []
+val tree_list : quadra_tree list ref = {contents = []}
+
+
+(* chapeau *)
+let x_cut bounds =
+    let tree = create_tree_list() in
+    begin
+
+x_cut_tree (bounds, tree);
+        tree;
+    end ;;
+
+(* normale *)
+let x_cut_tree ((x1, x2, y1, y2), tree_list) =
+    let tmp_y1 = (-1) in
+
+let y = ref y1 in
+
+begin
+
+while (y <= y2 + 1) do
+
+if (y = y2 +1 || x_hist (y, x1, x2) = 0) (* White line or out of bounds *)
+            then
+                if (tmp_y1 <> -1) then
+                begin
+
+                    boxes_tree := ((x1, x2, y1, y2), y_cut (x1, x2, tmp_y1, y) :: !tree_list);
+                    tmp_y1 := -1;
+
+end
+            else
+                if (tmp_y1 = -1) then
+                    tmp_y1 := y;
+
+done
+        y := y+1
+end ;;
