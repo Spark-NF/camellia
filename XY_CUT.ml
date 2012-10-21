1bbbd2f1f002ec8ab3f5c030ae37a8ab45ba8609
masternox <masternox@users.noreply.github.com>
2012-10-21 15:46:22 +0000

    -

--- XY_CUT.ml
+++ XY_CUT.ml
@@ -1,8 +1,6 @@
-
-
-(*  *)
+(* Returns the image size *)
 let img_size = (100, 100)
-
+;;
 
 (* Returns a boolean equals to true if the pixel is black *)
 let is_pixel_black x y img =
@@ -10,25 +8,34 @@ let is_pixel_black x y img =
 		color = (0, 0, 0)
 ;;
 
-(* Returns the number of black pixels in the line between the two x bounds *)
-let rec x_hist y x1 x2 img = match x1 with
-	| x2 -> 0
-	| x -> (if (is_pixel_black x y) then 1 else 0)
-		+ x_hist y (x+1) x2
+(* Returns the nb of black pixels in the line between the two x bounds *)
+let rec x_hist y x1 x2 img = if (x1 = x2) then 0
+	else(if (is_pixel_black x y img) then 1 else 0)
+		+ (x_hist y (x + 1) x2 img)
 ;;
 
-(* Returns the number of black pixels in the column between the two y bounds *)
+(* Returns the nb of black pixels in the column between the two y bounds *)
 let rec y_hist x y1 y2 img = match y1 with
-	| y2 -> 0
-	| y -> (if (is_pixel_black x y) then 1 else 0)
-		+ y_hist x (y+1) y2
+	| _ when y1 = y2 -> 0
+	| y -> (if (is_pixel_black x y img) then 1 else 0)
+		+ (y_hist x (y + 1) y2 img)
 ;;
 
-(* Type tree *)
-Type  quadra_tree = Empty | Node of (int, int, int, int) * quadra_tree list
+(* Type quadra_tree *)
+type quadra_tree =
+	| Empty
+	| Node of int * int * int * int * quadra_tree list
+;;
 
 (* Returns the limit under which we can consider the line being empty *)
-let w_line_limit = let (w, h) = img_size in w / 100
+let w_line_limit =
+	let (w, h) = img_size in
+	(w / 100)
+;;
+
+let y_cut (x1, x2, y1, y2) tmp_y1 order img =
+	img
+;;
 
 (*
 Returns the tree with the following caracteristics :
@@ -38,38 +45,39 @@ Returns the tree with the following caracteristics :
 	Put order to (-1) for to calculate until no separation is longer possible.
 *)
 let rec x_cut (x1, x2, y1, y2) tmp_y1 order img =
-	if (tmp_y1 = -1) then
+	if (tmp_y1 = (-1)) then
 	(* If not in black strip *)
-		if (y1 = y2 +1) then
+		(if (y1 = y2 + 1) then
 		(* If out of bounds*)
 			[]
-		else if (x_hist y x1 x2 < w_line_limit) then
+		else (if ((x_hist y1 x1 x2 img) < w_line_limit) then
 		(* If white line *)
-			x_cut (x1, x2, y1 +1, y2) -1 order img
-		else (* (x_hist y x1 x2 > w_line_limit) *)
+			x_cut (x1, x2, y1 +1, y2) (-1) order img
+			else (* (x_hist y1 x1 x2 img> w_line_limit) *)
 		(* If black line *)
-			x_cut (x1, x2, y1, y2) y1 order img
+			x_cut (x1, x2, y1, y2) y1 order img))
 	else (* (tmp_y1 <> -1) *)
 	(* If in black strip *)
-		if (y1 = y2 +1) then
+		(if (y1 = y2 + 1) then
 		(* If out of bounds *)
-			(
-				(x1, x2, tmp_y1, y1 -1),
-				if order > 0 then (y_cut (x1, x2, tmp_y1, y1 -1) -1 (order -1) img) else []
-			)
-			:: []
-		else if (x_hist y x1 x2 < w_line_limit) then
+			((
+				(x1, x2, tmp_y1, y1 - 1),
+				(if (order > 0)
+				then (y_cut (x1, x2, tmp_y1, y1 - 1) (-1) (order - 1) img)
+				else [])
+			):: [])
+		else (if (x_hist y1 x1 x2 img < w_line_limit) then
 		(* If white line *)
-			(
-				(x1, x2, tmp_y1, y1 -1),
-				if order > 0 then (y_cut (x1, x2, tmp_y1, y1 -1) -1 (order -1) img) else []
-			)
-			:: x_cut (x1, x2, y1, y2) -1 order img
-		else (* (x_hist y x1 x2 > w_line_limit) *)
+			((
+				(x1, x2, tmp_y1, y1 - 1),
+				(if (order > 0)
+				then (y_cut (x1, x2, tmp_y1, y1 - 1) (-1) (order - 1) img)
+				else [])
+			):: (x_cut (x1, x2, y1, y2) (-1) order img))
+			else (* (x_hist y1 x1 x2 img > w_line_limit) *)
 		(* If black line *)
-			[]
+			[]))
 ;;
-
 
 (* Changes the pixel color *)
 let change_pixel_color x y color img =
@@ -77,35 +85,42 @@ let change_pixel_color x y color img =
 ;;
 
 (* Draws the 4 lines  *)
-let rec draw_rect (x1, x2, y1, y2) img =
+(* let draw_rect (x1, x2, y1, y2) img =
 	begin
-		for i = x1 to x2 do
-			change_pixel_color i y1 Color.RED img;
-			change_pixel_color i y1 Color.RED img;
+	for i = x1 to x2 do
+		begin
+			Sdlvideo.put_pixel_color img i y1 (255, 0, 0);
+			Sdlvideo.put_pixel_color img i y2 (255, 0, 0);
+		end
 		done
 		for j = y1 to y2 do
-			change_pixel_color x1 j Color.RED img;
-			change_pixel_color x1 j Color.RED img;
-		done
+		begin
+			Sdlvideo.put_pixel_color img x1 j (255, 0, 0);
+			Sdlvideo.put_pixel_color img x2 j (255, 0, 0);
+		end
+	done
+	img;
 	end
 ;;
+*)
+
+let draw_rect (x1, x2, y1, y2) img = img;;
 
 (* Draw the rectangles around each bounds *)
-let rec draw_rects tree_list img = match tree_list with
-	| [] -> img
-	| (bounds, tree_list1) :: tree_list2 ->
+let rec draw_rects tree_list img = 
+	if (tree_list <> []) then
+		(let (bounds, tree_list1) :: tree_list2 = tree_list in
 		begin
-			draw_rect bounds img;
-			draw_rects tree_list1 img;
+			let img = draw_rect bounds img in
+			let img = draw_rects tree_list1 img in
 			draw_rects tree_list2 img;
-		end
+		end)
+	else (img)
 ;;
-
-
 
 (* Entry point *)
 let xy_cut img =
 	let (w, h) = img_size in
 	let order = 1 in
-	draw_rects [(x_cut (0, w, 0, h) -1 order img)] img
+	draw_rects [(x_cut (0, w, 0, h) (-1) order img)] img
 ;;
