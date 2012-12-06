3db52aee416deb62c8cc3480dde1fb279e772918
masternox <masternox@users.noreply.github.com>
2012-12-06 23:25:20 +0000

    Improvements to the neural network.

--- neuralnetwork.ml
+++ neuralnetwork.ml
@@ -12,28 +12,34 @@ class letter =
 		method get_x = x
 		method get_y = y
 		method get_mat = mat
+		(* NEW TYPE NEEDED TO CREATE A BIDIMENSIONAL MATRIX BELOW *)
+		(*
 		method rec create_pattern hashtab x y =
-			for i <- 0 to x do
+				for i <- 0 to x do
 				for j <- 0 to y do
 				Hashtabl.add hashtab i j
 	end
+	*)
+
 	
-let rec create_list boxes = match boxes with
 (* takes the boxes' list from XY-cut operation and create a letter list *)
+(*
+let rec create_list boxes = match boxes with
 		[] -> failwith "Internal error : no caracter detected"
 	| (x1,x2,y1,y2)::l -> let create_pattern x y = new letter x y in
 		(create_pattern (x2-x1) (y2-y1))::(create_list l)
-	
-let matching pat1 pat2 =
-	(* associates boxes from XY-cut operation to letter by filling letters with 0 and 1
-	and compares two matrices, returns an accuracy value and *)
-	
-	**** FIX ME ****
-	
+*)
 
-let first_try pattern matching_list =
+
+(* associates boxes from XY-cut operation to letter by filling letters with 0 and 1
+	and compares two matrices, returns an accuracy value and *)
+let matching pat1 pat2 =
+	(**** FIX ME ****)
+	
+	
 (* tries to match the given letter pattern with an entity from a database
 (an entire font for example) by calling the previous matching function *)
+let first_try pattern matching_list =
 	match matching_list with
 		[] -> failwith "Internal error : no database for matching"
 	| e::l -> matching pattern e;
--- treatment.ml
+++ treatment.ml
@@ -1,2 +1,22 @@
 (* This particular module will be used to optimize the XY-Cut and
 create a workable structure for the neural network *)
+
+(* open Hashtbl *) (* Uncomment if needed *)
+
+(* Type quadra_tree is here repeated *)
+type quadra_tree =
+	| Empty
+	| Node of int * int * int * int * (quadra_tree list) ;;
+
+(* This matrix is carrying the entire text of the page and
+ - Each letter is a box of this matrix
+ - Words are separated by an empty box
+ - A line of this matrix is a paragraph of the text *)
+let text_mat () =
+	let nb = ref (-1) (* The BMP title will then be used to separate words *)
+	and hshtb = Hashtbl.create 1 in
+	let build_mat quadra_tree = match quadra_tree with
+		| Node(x1, x2, y1, y2, tree_list) ->
+			Sdlvideo.save_BMP (Sdlvideo.rect x1 x2 y1 y2) string_of_int(!nb <- nb + 1)
+		| _ -> (**** FIX ME ****)
+			in (**** FIX ME ****)
