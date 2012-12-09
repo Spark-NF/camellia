423bd9852873bbcef8319dfe61eb1f00334a2450
masternox <masternox@users.noreply.github.com>
2012-12-09 19:59:07 +0000

    -

--- treatment.ml
+++ treatment.ml
@@ -5,9 +5,7 @@ type quadra_tree =
 
 (* Convertit un Sdlvideo.rect en histogramme de lettre *)
 let is_pixel_black x y img =
- if (Sdlvideo.get_pixel_color img x y = (0, 0, 0)) then 1 else 0 ;;
-
-(* Convertit un Sdlvideo.rect en histogramme de lettre *)
+	if (Sdlvideo.get_pixel_color img x y = (0, 0, 0)) then 1 else 0;;
 let rect_to_hist img =
 	let hist = ref [] and (w, h) = Sdlt.get_dims img in
 		begin
@@ -18,7 +16,18 @@ let rect_to_hist img =
 		end;;
 			
 (* Associe l'histogramme à un caractère *)
-let hist_to_char hist dico = 'c';;		
+let hist_to_char hist dico =
+	let accuracy = ref 2 in(* marge d'erreur de l'histogramme *)
+	let rec compare_hist_letter current_hist letter_hist(* du dico *) =
+		 match current_hist with
+			| []     -> failwith "No letter to match w/ database"
+			| e1::l1 -> match letter_hist with
+				| []     -> failwith "No database to match w/ letter"
+				| e2::l2 -> if (e1 <= e2+accuracy) & (e1 >= e2-accuracy) then
+					compare_hist_letter l1 l2 (* car la première ligne match *)
+							else (* Aller à la prochaine lettre du dico *)
+								(**** FIX ME PLEASE ****) in
+	compare_hist_letter hist (* Première lettre du dico *) (**** FIX ME PLEASE ****);;
 
 (* Convertit une liste d'arbres en liste récursive de listes de caractères *)
 let rec_build_mat dico l = function
@@ -26,7 +35,7 @@ let rec_build_mat dico l = function
 	| e::tree_list -> begin
 			build_mat dico l e;
 			rec_build_mat dico l tree_list;
-		end ;;
+		end;;
 
 (* Convertit un arbre en liste récursive de listes de caractères *)
 let build_mat dico l = function
