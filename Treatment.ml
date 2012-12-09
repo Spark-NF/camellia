39b2c6275a892084aeea0d4bde43dd7e8e06e080
masternox <masternox@users.noreply.github.com>
2012-12-09 23:29:16 +0000

    -

--- Treatment.ml
+++ Treatment.ml
@@ -6,6 +6,7 @@ type quadra_tree =
 (* Convertit un Sdlvideo.rect en histogramme *)
 let is_pixel_black x y img =
 	if (Sdlvideo.get_pixel_color img x y = (0, 0, 0)) then 1 else 0;;
+	
 let rect_to_hist img =
 	let hist = ref [] and (w, h) = Sdlt.get_dims img in
 		begin
@@ -25,30 +26,22 @@ let rec compare_hist_letter accuracy hist letter =
 				if (e1 <= e2+accuracy) && (e1 >= e2-accuracy) then
 					compare_hist_letter accuracy l1 l2
 				else
-					false ;;
+					false;;
 
 (* Cherche un histogramme dans une liste de paires (lettre, histogramme) *)
-let find_hist_letter accuracy x = function
+let rec find_hist_letter accuracy x = function
 	| [] -> '_'
 	| (letter_char, letter_hist)::l ->
 		if compare_hist_letter accuracy x letter_hist then
 			letter_char
 		else
-			find_hist_letter accuracy x l ;;
+			find_hist_letter accuracy x l;;
 
 let rect_to_char rect w h dico =
 	let hist = rect_to_hist rect in
 	let accuracy = ref 1 in (* marge d'erreur de l'histogramme *)
 	let format = if (float_of_int h) > (float_of_int h) then "high" else "small" in
-	find_hist_letter accuracy hist dico[format] ;;
-
-(* Convertit une liste d'arbres en liste récursive de listes de caractères *)
-let rec_build_mat dico l = function
-	| [] -> ()
-	| e::tree_list -> begin
-			build_mat dico l e;
-			rec_build_mat dico l tree_list;
-		end;;
+	find_hist_letter accuracy hist dico[format];;
 
 (* Convertit un arbre en liste récursive de listes de caractères *)
 let build_mat dico l = function
@@ -60,6 +53,14 @@ let build_mat dico l = function
 		end
 	| Node(_, _, _, _, tree_list) -> rec_build_mat dico l tree_list
 	| Empty -> ();;
+	
+(* Convertit une liste d'arbres en liste récursive de listes de caractères *)
+let rec rec_build_mat dico l = function
+	| [] -> []
+	| e::tree_list -> begin
+			build_mat dico l e;
+			rec_build_mat dico l tree_list;
+		end;;
 
 (* Renvoie récursivement une liste de listes des caractères de l'arbre *)
 let text_mat tree =
@@ -70,13 +71,13 @@ let text_mat tree =
 		Hashtbl.add dico "high" dico_high;
 		Hashtbl.add dico "small" dico_small;
 		build_mat dico lines tree;
-	end ;;
+	end;;
 
-	
-	
+
+
 			(* METHODE DE ZERNICKE *)
 
-			
+		
 (* Implémentation des moments géométriques (en coordonnées cartésiennes)
 Prend en paramètre un Sdlvideo.rect et renvoye une liste de moments *)
 let geo_mmt img =
@@ -95,12 +96,6 @@ let geo_mmt img =
 						moments := ((float_of_int(x)**counter_x) +. (float_of_int(y)**counter_y)) :: !moments;
 						return_mmt img (y-1) (x-1);
 						end;;
-
-
-
-
-
-
-
-
-
+						
+(* Implémentation des moments complexes (sous forme polynômiale complexe)
+Prend en paramètre un Sdlvideo.rect et renvoye une liste de moments *)
