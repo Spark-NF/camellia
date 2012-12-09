3118a36df945bb3f74913dd374fa18f748f1448a
masternox <masternox@users.noreply.github.com>
2012-12-09 22:53:35 +0000

    Start working on the Zernicke algorithm.

--- Treatment.ml
+++ Treatment.ml
@@ -86,13 +86,13 @@ let geo_mmt img =
 			| (1, 1) | (0, 0) 	-> moments
 			| (y, x) when y = 1 -> begin
 						counter_x := !counter_x +. 1.;
-						moments := ((x.**counter_x) + (y.**counter_y)) :: !moments;
+						moments := ((float_of_int(x)**counter_x) +. (float_of_int(y)**counter_y)) :: !moments;
 						return_mmt img y (x-1);
 						end
 			| (y, x) -> begin
 						counter_x := !counter_x +. 1.;
 						counter_y := !counter_y +. 1.;
-						moments := ((x.**counter_x) + (y.**counter_y)) :: !moments;
+						moments := ((float_of_int(x)**counter_x) +. (float_of_int(y)**counter_y)) :: !moments;
 						return_mmt img (y-1) (x-1);
 						end;;
 
